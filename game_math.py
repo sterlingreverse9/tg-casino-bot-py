816941c411ccde40320629aa1385c3834b224600
sterlingreverse9 <sterlingreverse9@gmail.com>
2026-07-31 12:36:29 +0530

    Update game_math.py

--- game_math.py
+++ game_math.py
@@ -1,14 +1,12 @@
-from config import HOUSE_EDGE
+from settings import get_house_edge
 
 
 def multiplier_for(win_chance: float) -> float:
     """
     Payout multiplier for a given true win probability.
-    Matches spec: 50% win chance, 0.10 edge -> multiplier 1.9
-    (bet 10 -> win 19)
     multiplier = (1 / win_chance) - house_edge
     """
-    return (1 / win_chance) - HOUSE_EDGE
+    return (1 / win_chance) - get_house_edge()
 
 
 def payout_for(bet_amount: float, win_chance: float) -> float:
