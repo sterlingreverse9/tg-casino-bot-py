c7f15f8921f56a96d2a051fd0c77840a7b7d55d4
sterlingreverse9 <sterlingreverse9@gmail.com>
2026-08-02 06:48:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import handlers.basic
 import handlers.admin
 import handlers.rain
 import handlers.deposit
+import handlers.withdraw
 import handlers.games
 import handlers.dice_duel
 import handlers.tower
+import handlers.referral
+import handlers.tracking
 
 print(f"{CASINO_NAME} bot running...")
 bot.infinity_polling()
