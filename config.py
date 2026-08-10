702fbda63eaea782bf4dfa934612f8c1bf3a4a50
sterlingreverse9 <sterlingreverse9@gmail.com>
2026-08-10 15:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8305704380:AAEYFpfvZdjV2Od5MDaD4mdeeb-f--XmRVk")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
